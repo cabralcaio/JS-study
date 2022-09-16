8fe99cffdf1d33197555231d207aff7754a854a2
cabralcaio <caio_cabral1@hotmail.com>
2022-09-16 12:01:08 -0300

    aula2

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * -> Tipos primitivos
+ * string : Textos e caracteres
+ * number : números (inteiros e decimais)
+ * boolean : booleano (verdadeiro ou falso)
+ * 
+ * -> Variáveis
+ * -> Operadores matemáticos
+ */
+
+//Serve para escrever msg no console
+console.log("hello world")
+console.log(5)
+console.log(43.73)
+console.log(true)
+console.log(false)
+
+
+
+//Variáveis
+
+let mensagem = 'SoulCode BC 25 JFS'
+
+
+console.log(mensagem) //SoulCode BC 25 JFS 
+mensagem = 'Novo texto'
+
+console.log(mensagem) //Novo texto
